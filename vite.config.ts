f904602a50473aa0f280b7295b30e695b9a7f848
Raushan Iangirov <mandarinko999@gmail.com>
2024-10-13 23:09:17 +0300

    adding dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	base: '/todos-vue-pinia-ts/',
 	plugins: [vue()],
 	build: {
 		target: 'esnext',
